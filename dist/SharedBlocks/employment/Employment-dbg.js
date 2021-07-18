bfb887ae27678cc7d70629457c4d4c839cbf516a
juanfelipe82193 <juan19_93@hotmail.com>
2021-07-18 12:41:34 -0500

    Projects

---
+++ dist/SharedBlocks/employment/Employment-dbg.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/uxap/BlockBase"
+], function (
+    BlockBase
+) {
+    "use strict";
+
+    var Employment = BlockBase.extend("my.page.object.SharedBlocks.employment.Employment", {
+        metadata: {}
+    });
+    return Employment;
+});
